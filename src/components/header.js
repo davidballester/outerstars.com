38e14fd27073be9480979d3956da300d7e0ebd31
David Ballester Mena <david.ballester.mena@gmail.com>
2020-05-09 13:07:15 +0200

    refactor: move header to its own component

---
+++ src/components/header.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+const MainPageHeader = ({ title }) => {
+  return (
+    <h1
+      style={{
+        ...scale(1.5),
+        marginBottom: rhythm(1.5),
+        marginTop: 0,
+      }}
+    >
+      <Link
+        style={{
+          boxShadow: `none`,
+          color: `inherit`,
+        }}
+        to={`/`}
+      >
+        {title}
+      </Link>
+    </h1>
+  )
+}
+
+const SecondaryPageHeader = ({ title }) => {
+  return (
+    <h3
+      style={{
+        fontFamily: `Montserrat, sans-serif`,
+        marginTop: 0,
+      }}
+    >
+      <Link
+        style={{
+          boxShadow: `none`,
+          color: `inherit`,
+        }}
+        to={`/`}
+      >
+        {title}
+      </Link>
+    </h3>
+  )
+}
+
+const Header = ({ location, title }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const DisplayHeader =
+    location.pathname === rootPath ? MainPageHeader : SecondaryPageHeader
+  return (
+    <header>
+      <DisplayHeader title={title} />
+    </header>
+  )
+}
+
+export default Header
