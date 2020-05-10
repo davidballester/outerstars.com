263b61cb6c0da53792f5ae9334c7524103c354bd
David Ballester Mena <david.ballester.mena@gmail.com>
2020-05-10 14:30:51 +0200

    refactor: extract post thumbnail to a separate component

--- src/components/postList.js
+++ src/components/postList.js
@@ -1,42 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Bio from "./bio"
 import Layout from "./layout"
 import SEO from "./seo"
-import { rhythm } from "../utils/typography"
+import PostThumbnail from "./postThumbnail"
 
 const PostList = ({ siteTitle, posts, location, linkToRoot }) => {
   return (
     <Layout location={location} title={siteTitle} linkToRoot={linkToRoot}>
       <SEO title="All posts" />
       <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
+      {posts.map(({ node }) => (
+        <PostThumbnail key={node.fields.slug} node={node} />
+      ))}
     </Layout>
   )
 }
